7b23b931de7eca06e04233dc41841e62f30e7d26
iiPython <ben@iipython.cf>
2023-04-21 16:17:37 -0500

    An API that actually functions!

--- src/app.py
+++ src/app.py
@@ -1,10 +1,27 @@
 # Copyright 2023 iiPython
 
 # Modules
+import asyncio
 from blacksheep import Application
+
+from .config import config
+from .storage import Scraper
 
 # Initialization
 app = Application()
+
+# Handle periodic scraping
+scraper = Scraper()  # noqa
+async def scrape_task(app: Application) -> None:
+    interval = config.get("scrape_interval", 10) * 60
+    while True:
+        scraper.scrape_weather()
+        await asyncio.sleep(interval)
+
+async def configure_scraper(app: Application) -> None:
+    asyncio.get_event_loop().create_task(scrape_task(app))
+
+app.on_start += configure_scraper
 
 # Routes
 from .routes import (api, frontend)  # noqa
--- src/routes/api.py
+++ src/routes/api.py
@@ -1,1 +1,21 @@
 # Copyright 2023 iiPython
+
+# Modules
+import re
+from src.app import app, scraper
+
+# Initialization
+date_whitelist = r"[^0-9\-]"
+
+# Routes
+@app.route("/api/now", methods = ["GET"])
+async def get_api_now() -> dict:
+    return {
+        "status": 200,
+        "data": scraper.get_weather_for(scraper.current_key()[:-5])[-1]  # :-5 removes .json
+    }
+
+@app.route("/api/past/{date}", methods = ["GET"])
+async def get_api_past(date: str) -> dict:
+    data = scraper.get_weather_for(re.sub(date_whitelist, "", date))
+    return {"status": 200 if data is not None else 404, "data": data}
